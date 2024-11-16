c2e7ed4cb38f11deb65d08ef0c60842881a83529
Daniel <Developer_Daniel.@qq.com>
2024-11-16 08:34:50 +0800

    commit

--- src/QTApplication.cpp
+++ src/QTApplication.cpp
@@ -1,25 +1,13 @@
 #include "QTApplication.h"
 #include <QApplication>
+#include <QPushButton>
 
 QTApplication::QTApplication(QWidget *parent) : QMainWindow(parent), ui(new Ui_QTApplication)
 {
     ui->setupUi(this);
-
-    connect(ui->commitLineEdit, SIGNAL(returnPressed()), this, SLOT(on_commitButton_clicked()));
 }
 
 QTApplication::~QTApplication()
 {
     delete ui;
-}
-
-void QTApplication::on_commitButton_clicked()
-{
-
-    QString context = ui->commitLineEdit->text();
-
-    ui->label->setText(context);
-    ui->commitLineEdit->setText("");
-    //QProcess *process = new QProcess(this);
-    //process->start(context);
 }
--- src/QTApplication.h
+++ src/QTApplication.h
@@ -1,19 +1,20 @@
 #pragma once
 #include "ui_QTApplication.h"
 #include <QMainWindow>
+#include <QException>
 #include <QProcess>
 
-class QTApplication : public QMainWindow
+class QTApplication : public QMainWindow, QException
 {
     Q_OBJECT
 
 public:
     QTApplication(QWidget *parent = nullptr);
     ~QTApplication();
+    
 
 private:
     Ui_QTApplication *ui;
 
 private slots:
-    void on_commitButton_clicked();
 };
