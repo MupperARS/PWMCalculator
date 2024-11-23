1fe74ad8309bd190adbd777bba93eda629e9de07
Daniel <Developer_Daniel.@qq.com>
2024-11-23 18:36:04 +0800

    commit

--- src/QTApplication.cpp
+++ src/QTApplication.cpp
@@ -3,29 +3,54 @@
 
 QTApplication::QTApplication(QWidget *parent) : QMainWindow(parent), ui(new Ui_QTApplication)
 {
-    this->setWindowIcon(QIcon("../static/calculator.ico"));
-    this->setWindowTitle("计算器");
-    this->setMinimumSize(320, 500);
+    int count = 0;
+    QString buttonName[] = {"7", "8", "9", "4", "5", "6", "1", "2", "3", "±", "0", "."};
+    QSignalMapper *mapper = new QSignalMapper(this);
 
     ui->setupUi(this);
     // setFont
     ui->mainLineEdit->setFont(QFont("仿宋", 26));
     // setButtonColor
-    ui->equalButton->setStyleSheet("background:#65A5D1");
-    ui->mainLineEdit->setText("0");
+    ui->EqualButton->setStyleSheet("background:#65A5D1");
+    ui->mainLineEdit->setText(expression = "0");
 
-    QSignalMapper *mapper = new QSignalMapper(this);
-    connect(mapper, SIGNAL(mappedString(const QString &)), this, SLOT(doClicked(const QString &)));
-    for (int i = 0; i < 3; i++)
+    this->setWindowIcon(QIcon("../static/calculator.ico"));
+    this->setWindowTitle("计算器");
+    this->setMinimumSize(320, 500);
+
+    mapper->setMapping(ui->EqualButton, 10);
+    mapper->setMapping(ui->Addition, 11);
+    mapper->setMapping(ui->Subtraction, 12);
+    mapper->setMapping(ui->Multiplication, 13);
+    mapper->setMapping(ui->Division, 14);
+    mapper->setMapping(ui->Backspace, 15);
+    mapper->setMapping(ui->Clear, 16);
+
+    connect(ui->EqualButton, SIGNAL(clicked()), mapper, SLOT(map()));
+    connect(ui->Addition, SIGNAL(clicked()), mapper, SLOT(map()));
+    connect(ui->Subtraction, SIGNAL(clicked()), mapper, SLOT(map()));
+    connect(ui->Multiplication, SIGNAL(clicked()), mapper, SLOT(map()));
+    connect(ui->Division, SIGNAL(clicked()), mapper, SLOT(map()));
+    connect(ui->Backspace, SIGNAL(clicked()), mapper, SLOT(map()));
+    connect(ui->Clear, SIGNAL(clicked()), mapper, SLOT(map()));
+
+    for (int i = 1; i < 5; i++)
     {
-        numberButton[i] = new QPushButton("Button" + QString::number(i), this);
-        numberButton[i]->setMinimumSize(60, 75);
-        numberButton[i]->setText(QString::number(i));
+        for (int j = 0; j < 3; j++)
+        {
+            numberButton[count] = new QPushButton("Button" + QString::number(i), this);
+            numberButton[count]->setMinimumSize(75, 75);
+            numberButton[count]->setMaximumSize(75, 75);
+            numberButton[count]->setText(buttonName[count]);
 
-        ui->gridLayout->addWidget(numberButton[i], 2, i);
-        mapper->setMapping(numberButton[i], QString::number(i));
-        connect(numberButton[i], SIGNAL(clicked()), mapper, SLOT(map()));
+            ui->gridLayout->addWidget(numberButton[count], i, j);
+            mapper->setMapping(numberButton[count], buttonName[count]);
+            connect(numberButton[count], SIGNAL(clicked()), mapper, SLOT(map()));
+            count++;
+        }
     }
+    connect(mapper, SIGNAL(mappedString(const QString &)), this, SLOT(numberClicked(const QString &)));
+    connect(mapper, SIGNAL(mappedInt(int)), this, SLOT(ctrlClicked(int)));
 }
 
 QTApplication::~QTApplication()
@@ -33,14 +58,91 @@ QTApplication::~QTApplication()
     delete ui;
 }
 
-void QTApplication::doClicked(const QString &text)
+double QTApplication::combine(double Compute, int dec)
 {
-    if (ui->mainLineEdit->text().toInt() != 0)
+    result = Compute;
+    decimal = dec;
+    return result;
+}
+
+void QTApplication::numberClicked(const QString &text)
+{
+    int id = text.toInt();
+    switch (methood)
     {
-        expression += text;
+    case 11:
+        decimal = 1;
+        combine(result + id * decimal, decimal * 10);
+        break;
+    case 12:
+        decimal = 1;
+        combine(result - id * decimal, decimal * 10);
+        break;
+    case 13:
+        decimal = 1;
+        result *= text.toInt(); // x
+        break;
+    case 14:
+        decimal = 1;
+        result /= text.toInt(); // ÷
+        break;
+    default:
+        int value = ui->mainLineEdit->text().at(0).digitValue();
+        result = value ? combine(id + result * decimal * 10, decimal * 10) : id;
+        expression = value ? expression + text : text;
+        break;
     }
-    else
+    if (methood)
     {
+        methoodCount--;
+        methood = 0;
+        expression.append(text);
     }
+
+    ui->mainLineEdit->setText(expression);
+}
+
+void QTApplication::ctrlClicked(int value)
+{
+    if (!expression.back().digitValue())
+        return;
+    methood = value;
+    methoodCount++;
+    switch (value)
+    {
+    case 10:
+        expression.append("=" + QString::number(result)); //=
+        break;
+    case 11:
+        expression.append("+"); //+
+        break;
+    case 12:
+        expression.append("-"); //-
+        break;
+    case 13:
+        expression.append("x"); // x
+        break;
+    case 14:
+        expression.append("÷"); // ÷
+        break;
+    case 15:
+        // Backspace
+        if (expression.length() > 1)
+        {
+            expression = expression.chopped(1);
+            break;
+        }
+        result = 0;
+        methood = 0;
+        expression = "0";
+        break;
+    case 16:
+        result = 0;
+        methood = 0;
+        decimal = 1;
+        expression = "0";
+        break;
+    }
+
     ui->mainLineEdit->setText(expression);
 }
--- src/QTApplication.h
+++ src/QTApplication.h
@@ -1,22 +1,28 @@
 #pragma once
 #include "ui_QTApplication.h"
 #include <QMainWindow>
-#include <iostream>
 #include <QSignalMapper>
 
 class QTApplication : public QMainWindow
 {
     Q_OBJECT
+private:
+    Ui_QTApplication *ui;
+    QPushButton *numberButton[13];
+    QString expression;
+    double result;
+    int decimal = 1;
+    int methood = 0;
+    int methoodCount = 0;
 
 public:
     explicit QTApplication(QWidget *parent = nullptr);
     ~QTApplication();
 
 private:
-    Ui_QTApplication *ui;
-    QString expression;
-    QPushButton *numberButton[9];
+    double combine(double Compute, int dec);
 
 private slots:
-    void doClicked(const QString &text);
+    void ctrlClicked(int);
+    void numberClicked(const QString &);
 };
