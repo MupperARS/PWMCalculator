a0e89703b0192b24dce893a417554ddf80eb4cb3
Daniel <Developer_Daniel.@qq.com>
2025-01-18 12:47:07 +0800

    commit

--- src/QTApplication.cpp
+++ src/QTApplication.cpp
@@ -20,25 +20,33 @@ QTApplication::QTApplication(QWidget *parent) : QMainWindow(parent), ui(new Ui_Q
         lineEdit[i] = new QLineEdit(this);
         label[i]->setText(name[i]);
         label[i]->setMinimumSize(10, 15);
-        lineEdit[i]->setMinimumSize(60, 15);
-        lineEdit[i]->setMaximumWidth(60);
+        lineEdit[i]->setMinimumSize(200, 15);
         ui->gridLayout->addWidget(label[i]);
         ui->gridLayout->addWidget(lineEdit[i]);
         if (i == 1)
         {
             comboBox = new QComboBox(this);
             comboBox->addItems(list);
-            comboBox->setMaximumWidth(40);
             ui->gridLayout->addWidget(comboBox);
         }
-
         mapper->setMapping(lineEdit[i], i);
         connect(lineEdit[i], SIGNAL(textChanged(const QString &)), mapper, SLOT(map()));
     }
+    label[4] = new QLabel("PWM:", this);
+    label[5] = new QLabel("", this);
+
+    label[5]->setMinimumHeight(50);
+    label[5]->setStyleSheet({"background-color: #FFF; border:1px solid black;"});
+    QFont font;
+    font.setPointSize(24);
+    label[5]->setFont(font);
+
+    ui->gridLayout->addWidget(label[4]);
+    ui->gridLayout->addWidget(label[5]);
 
     connect(mapper, SIGNAL(mappedInt(int)), this, SLOT(onChange(int)));
+    connect(comboBox, SIGNAL(textActivated(const QString &)), this, SLOT(onComboChange(const QString &)));
 }
-// PWM分频
 /**
  * @brief 析构函数
  */
@@ -47,22 +55,49 @@ QTApplication::~QTApplication()
     delete ui;
 }
 
+void QTApplication::calculator()
+{
+    if (core != 0 && Prescaler != 0 && resetCount != 0)
+    {
+        PWM = core / (Prescaler * resetCount);
+        if (PWM != 0)
+        {
+            label[5]->setText(QString::number(PWM));
+            return;
+        }
+        label[5]->setText("");
+    }
+}
+
+/**
+ * @brief 槽函数
+ * @param text 字符串
+ */
+void QTApplication::onComboChange(const QString &text)
+{
+    lineEdit[1]->blockSignals(true);
+    lineEdit[1]->setText(text);
+    Prescaler = text.back().digitValue();
+    lineEdit[1]->blockSignals(false);
+    calculator();
+}
+
 /**
  * 槽函数
  * @param value 枚举或数字
  */
 void QTApplication::onChange(int vlaue)
 {
-    qDebug() << vlaue;
     switch (vlaue)
     {
     case 0:
         /* 核心频率(MHz) */
-        core = lineEdit[0]->text().toUInt();
+        core = lineEdit[0]->text().toUInt() * 1000;
         break;
     case 1:
         /* PWM分频 */
         Prescaler = lineEdit[1]->text().toUInt();
+
         break;
     case 2:
         /* 装载值 */
@@ -75,4 +110,5 @@ void QTApplication::onChange(int vlaue)
     default:
         break;
     }
+    calculator();
 }
--- src/QTApplication.h
+++ src/QTApplication.h
@@ -2,7 +2,6 @@
 #include "ui_QTApplication.h"
 #include <QMainWindow>
 #include <QSignalMapper>
-#include <QVBoxLayout>
 #include <QLineEdit>
 #include <QLabel>
 #include <QComboBox>
@@ -12,21 +11,23 @@ class QTApplication : public QMainWindow
     Q_OBJECT
 private:
     Ui_QTApplication *ui;
-    QLabel *label[4];
+    QLabel *label[6];
     QLineEdit *lineEdit[4];
     QComboBox *comboBox;
-    uint16_t core;          //核心数
-    uint16_t PWM;           //pwm
-    uint16_t resetCount;    //计数
-    uint16_t Prescaler;     //预分频系数
-    uint16_t pulse;         //
+    uint16_t core = 0U;          //核心数
+    uint16_t PWM = 0U;           //pwm
+    uint16_t resetCount = 0U;    //计数
+    uint16_t Prescaler = 0U;     //预分频系数
+    uint16_t pulse = 0U;         //
 
 public:
     explicit QTApplication(QWidget *parent = nullptr);
     ~QTApplication();
 
+    void calculator();
 private:
 
 private slots:
     void onChange(int vlaue);
+    void onComboChange(const QString &text);
 };
